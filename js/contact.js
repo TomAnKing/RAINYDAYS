58fa04b6d25e38be92c7045232fd475e35598e11
TomAnKing <71315446+TomAnKing@users.noreply.github.com>
2021-03-19 14:34:45 +0100

    added form to contact

---
+++ js/contact.js
@@ -0,0 +1,44 @@
+const form = document.querySelector("#contactForm");
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailError");
+const subject = document.querySelector("#subject");
+const subjectError = document.querySelector("#subjectError");
+
+function validateForm(event) {
+  event.preventDefault();
+
+  let success = true;
+
+  if (validateEmail(email.value)) {
+    emailError.style.display = "none";
+  } else {
+    emailError.style.display = "block";
+    success = false;
+  }
+
+  if (checkLength(subject.value, 10) === true) {
+    subjectError.style.display = "none";
+  } else {
+    subjectError.style.display = "block";
+    success = false;
+  }
+
+  if (success) {
+  }
+}
+
+form.addEventListener("submit", validateForm);
+
+function checkLength(value, len) {
+  if (value.trim().length >= len) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validateEmail(email) {
+  const regEx = /\S+@\S+\.\S+/;
+  const patternMatches = regEx.test(email);
+  return patternMatches;
+}
---
+++ js/register.js
@@ -0,0 +1,63 @@
+const form = document.querySelector("#registerForm");
+const firstName = document.querySelector("#firstName");
+const firstNameError = document.querySelector("#firstNameError");
+const sirName = document.querySelector("#sirName");
+const sirNameError = document.querySelector("#sirNameError");
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailError");
+const password = document.querySelector("#password");
+const passwordError = document.querySelector("#passwordError");
+
+function validateForm(event) {
+  event.preventDefault();
+
+  let success = true;
+
+  if (checkLength(firstName.value, 1) === true) {
+    firstNameError.style.display = "none";
+  } else {
+    firstNameError.style.display = "block";
+    success = false;
+  }
+
+  if (checkLength(sirName.value, 1) === true) {
+    sirNameError.style.display = "none";
+  } else {
+    sirNameError.style.display = "block";
+    success = false;
+  }
+
+  if (validateEmail(email.value)) {
+    emailError.style.display = "none";
+  } else {
+    emailError.style.display = "block";
+    success = false;
+  }
+
+  if (checkLength(password.value, 8) === true) {
+    passwordError.style.display = "none";
+  } else {
+    passwordError.style.display = "block";
+    success = false;
+  }
+
+  if (success) {
+    window.location.href = "../chekout.html";
+  }
+}
+
+form.addEventListener("submit", validateForm);
+
+function checkLength(value, len) {
+  if (value.trim().length >= len) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validateEmail(email) {
+  const regEx = /\S+@\S+\.\S+/;
+  const patternMatches = regEx.test(email);
+  return patternMatches;
+}
